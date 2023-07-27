5bc522d37560ebc990a43b09a6b2d9d4502d7afe
Artem Sibisov <artsib7777@yandex.ru>
2023-07-28 00:47:57 +0800

    Переимновать БД на bitfilmsdb

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 
-const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 const app = express();
 
