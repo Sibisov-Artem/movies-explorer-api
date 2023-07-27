faf5bbba4b2f078f40ac53c07cf4ae159123a278
Artem Sibisov <artsib7777@yandex.ru>
2023-07-28 00:36:43 +0800

    Подключиться в app.js к серверу MongoDB по адресу: mongodb://localhost:27017/mestodb

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
+
+mongoose.connect(DB_URL, {
+}).then(() => {
+  console.log('connected to DB');
+});
 
 app.listen(PORT, () => {
   console.log(`Приложение слушает следующий порт: ${PORT}`);
