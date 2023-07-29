c876496f60c7d53f5cd5f63dd4cd313b1cf130b9
Artem Sibisov <artsib7777@yandex.ru>
2023-07-29 16:51:13 +0800

    Создать controller movies и накидать предварительные функции-контроллеры: getMovies, createMovie, deleteMovie

---
+++ controller/movies.js
@@ -0,0 +1,46 @@
+const Movie = require('../models/movies');
+
+// возвращает все сохранённые текущим пользователем фильмы
+const getMovies = (req, res) => {
+  Movie.find({})
+    .then((movies) => res.send({ data: movies }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+// создаёт фильм с переданными в теле
+// country, director, duration, year, description, image, trailer, nameRU, nameEN и thumbnail, movieId
+const createMovie = (req, res) => {
+  console.log(req.body);
+  const { country,
+    director, duration,
+    year, description,
+    image, trailer,
+    nameRU, nameEN,
+    thumbnail, movieId
+  } = req.body;
+  Movie.create({
+    country, director,
+    duration, year,
+    description, image,
+    trailer, nameRU,
+    nameEN, thumbnail,
+    movieId,
+    owner: req.user._id
+  })
+    .then((movie) => res.send({ data: movie }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+// удаляет сохранённый фильм по id
+// DELETE /movies/_id
+const deleteMovie = (req, res) => {
+  Movie.findByIdAndRemove(req.params.movieId)
+    .then((movie) => res.send({ data: movie }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports = {
+  getMovies,
+  createMovie,
+  deleteMovie,
+};
