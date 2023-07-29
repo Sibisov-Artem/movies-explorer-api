0d1b8aeca6250e1ca00fb3bd7992620899c0d2d3
Artem Sibisov <artsib7777@yandex.ru>
2023-07-29 19:18:24 +0800

    Создать routes movies: для получения фильмов пользователя, для добавления фильмов пользователя, для удаления сохраненных фильмов пользователя; подключить эти роуты к app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/users');
+const movieRoutes = require('./routes/movies');
+
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
@@ -13,6 +15,7 @@ mongoose.connect(DB_URL, {
 });
 
 app.use('/users', userRoutes);
+app.use('/movies', movieRoutes);
 
 app.listen(PORT, () => {
   console.log(`Приложение слушает следующий порт: ${PORT}`);
---
+++ routes/movies.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const { getMovies, createMovie, deleteMovie } = require('../controller/movies');
+
+// возвращает все сохранённые текущим пользователем фильмы
+router.get('/', getMovies);
+
+// создаёт фильм
+router.post('/', createMovie);
+
+//удаляет сохранённый фильм по id
+router.delete('/_id', deleteMovie);
+
+module.exports = router;
