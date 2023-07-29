ac625d356c53e477ef516d9e92c62c495bd91795
Artem Sibisov <artsib7777@yandex.ru>
2023-07-29 19:01:37 +0800

    Создать routes users: для получения информации о пользователе, обновления информации о пользователе; подключение этих роутов к app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const userRoutes = require('./routes/users');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
@@ -10,6 +11,8 @@ mongoose.connect(DB_URL, {
 }).then(() => {
   console.log('connected to DB');
 });
+
+app.use('/users', userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Приложение слушает следующий порт: ${PORT}`);
---
+++ routes/users.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const { getCurrentUser, updateUserInfoById, } = require('../controllers/users');
+
+// возвращает информацию о пользователе (email и имя)
+router.get('/me', getCurrentUser);
+
+// обновляет информацию о пользователе (email и имя)
+router.patch('/me', updateUserInfoById);
+
+module.exports = router;
