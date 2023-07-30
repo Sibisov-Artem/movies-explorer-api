f4ef4a10974968773ca4bb0b5d7f8cccf06d0a08
Artem Sibisov <artsib7777@yandex.ru>
2023-07-31 01:15:11 +0800

    Заменить bodyParser.json() на express.json(). После замены удалить пакет из зависимостей

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const router = require('./routes/index');
-const bodyParser = require('body-parser');
 const errorHandler = require('./middlewares/errorsHandler');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
@@ -14,7 +13,7 @@ mongoose.connect(DB_URL, {
   console.log('connected to DB');
 });
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use(router);
 
