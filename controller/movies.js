353eaa48c7dc7449f35e9e6546303b8b2da151ff
Artem Sibisov <artsib7777@yandex.ru>
2023-07-31 00:34:57 +0800

    Добавить обработку ошибок в controller movies, установить запрет на удаление чужих видео

--- controller/movies.js
+++ controller/movies.js
@@ -1,22 +1,26 @@
 const Movie = require('../models/movie');
 
+const BadRequestError = require('../utils/errors/BadRequestError'); // 400
+const NotFoundError = require('../utils/errors/NotFoundError'); // 404
+const ForbiddenError = require('../utils/errors/ForbiddenError'); // 403
+
 // возвращает все сохранённые текущим пользователем фильмы
-const getMovies = (req, res) => {
+const getMovies = (req, res, next) => {
   Movie.find({})
     .then((movies) => res.send({ data: movies }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch(next);
 };
 
 // создаёт фильм с переданными в теле
 // country, director, duration, year,
 // description, image, trailer, nameRU, nameEN и thumbnail, movieId
-const createMovie = (req, res) => {
+const createMovie = (req, res, next) => {
   console.log(req.body);
   const {
     country,
     director, duration,
     year, description,
-    image, trailer,
+    image, trailerLink,
     nameRU, nameEN,
     thumbnail, movieId,
   } = req.body;
@@ -27,23 +31,46 @@ const createMovie = (req, res) => {
     year,
     description,
     image,
-    trailer,
+    trailerLink,
     nameRU,
     nameEN,
     thumbnail,
     movieId,
     owner: req.user._id,
   })
-    .then((movie) => res.send({ data: movie }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((movie) => res.status(201).send({ data: movie }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new BadRequestError('При создании переданы некорректные данные.'));
+      } else {
+        next(err);
+      }
+    });
 };
 
 // удаляет сохранённый фильм по id
 // DELETE /movies/_id
-const deleteMovie = (req, res) => {
-  Movie.findByIdAndRemove(req.params.movieId)
-    .then((movie) => res.send({ data: movie }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+const deleteMovie = (req, res, next) => {
+  Movie.findById(req.params.movieId)
+    .then((movie) => {
+      if (!movie) {
+        return next(new NotFoundError('Фильм с указанным _id не найден.'));
+      }
+      if (req.user._id !== movie.owner.toString()) {
+        return next(new ForbiddenError('Удалять фильмы других пользователей недопустимо'));
+      }
+      return movie.deleteOne()
+        .then((movieDelete) => {
+          res.send({ data: movieDelete });
+        });
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new BadRequestError('Удаление фильма с некорректным id.'));
+      } else {
+        next(err);
+      }
+    });
 };
 
 module.exports = {
