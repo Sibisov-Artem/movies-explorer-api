232399dfa09955418c6ad80b6c438c39de657ec7
Artem Sibisov <artsib7777@yandex.ru>
2023-07-31 00:50:41 +0800

    Закрыть доступ на другие роуты ошибкой NotFoundError в index.js route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
+
 const userRoutes = require('./users');
 const movieRoutes = require('./movies');
 const authRoutes = require('./authentication');
 const auth = require('../middlewares/auth');
+
+const NotFoundError = require('../utils/errors/NotFoundError');
 
 router.use(authRoutes);
 
@@ -10,5 +13,9 @@ router.use(auth);
 
 router.use('/users', userRoutes);
 router.use('/movies', movieRoutes);
+
+router.use((req, res, next) => {
+  next(new NotFoundError('Здесь ничего нет'));
+});
 
 module.exports = router;
