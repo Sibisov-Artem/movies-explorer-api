6b7cacc00f506ea52804926b74baf14cb818edf5
Artem Sibisov <artsib7777@yandex.ru>
2023-08-01 22:22:16 +0800

    Подключить лимитер запросов express-rate-limit для ограничения количества запросов к API и предотвращения перегрузки сервера или снижения производительности приложения

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 
 const router = require('./routes/index');
 const errorHandler = require('./middlewares/errorsHandler');
@@ -12,7 +13,18 @@ const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process
 
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // снятие ограничения через 5 минут
+  max: 5, // Лимит на запрос с каждого IP до 5 запросов на `window` (здесь на 5 минут)
+  message:
+    'Слишком много запросов с этого IP, пожалуйста, повторите попытку через 5 минут',
+  standardHeaders: true, // Возвращаем информацию о лимите скорости в заголовках `RateLimit-*`
+  legacyHeaders: false, // Отключаем заголовки `X-RateLimit-*`
+});
+
 app.use(helmet());
+
+app.use(limiter);
 
 mongoose.connect(DB_URL, {
 }).then(() => {
