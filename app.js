fd5b85754237f8f26476ead926621295dc9b8446
Artem Sibisov <artsib7777@yandex.ru>
2023-08-02 21:48:38 +0800

    fix: Расположить limiter после логгера запросов, иначе запросы, отклоненные лимитером, не будут добавлены в лог запросов.

--- app.js
+++ app.js
@@ -14,8 +14,6 @@ const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process
 
 const app = express();
 
-app.use(helmet());
-
 app.use(cors());
 
 mongoose.connect(DB_URL, {
@@ -26,6 +24,8 @@ mongoose.connect(DB_URL, {
 app.use(express.json());
 
 app.use(requestLogger);
+
+app.use(helmet());
 
 app.use(limiter);
 
